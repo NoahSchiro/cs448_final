747585499784eb31cf386324c0d18a432d5cf25d
Kofwana <kofwanlawson@gmail.com>
2023-11-10 22:53:40 -0500

    rnn model

--- src/rnn.py
+++ src/rnn.py
@@ -1,8 +1,90 @@
+import pandas as pd
+import torch
 from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tqdm import tqdm
+from sklearn.feature_extraction.text import CountVectorizer
 
+from utils import load_data, clean_text, additional_cleaning
+
+# Load the dataset
+file_path = '../cs448_final/data/data.csv'
+df = load_data(file_path)
+
+
+df['text'] = df['text'].progress_apply(clean_text)
+df['text'] = df['text'].progress_apply(additional_cleaning)
+
+
+# Map labels to integers
+label_encoder = LabelEncoder()
+df['target'] = label_encoder.fit_transform(df['target'])
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(df['text'], df['target'], test_size=0.2, random_state=42)
+
+# Tokenize and convert text data to numerical representations using CountVectorizer
+vectorizer = CountVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Convert to PyTorch tensors
+X_train = torch.tensor(X_train.toarray(), dtype=torch.float32)
+y_train = torch.tensor(y_train.values, dtype=torch.float32)
+X_test = torch.tensor(X_test.toarray(), dtype=torch.float32)
+y_test = torch.tensor(y_test.values, dtype=torch.float32)
+
+# Build the RNN model
 class RNNModel(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNNModel, self).__init__()
+        self.embedding = nn.EmbeddingBag(input_size, hidden_size, sparse=True)
+        self.rnn = nn.LSTM(hidden_size, hidden_size)
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        pass
+        embedded = self.embedding(x)
+        output, _ = self.rnn(embedded.view(len(x), 1, -1))
+        output = self.fc(output.view(len(x), -1))
+        output = self.sigmoid(output)
+        return output
+
+# Initialize the model
+vocabulary_size = X_train.shape[1]
+hidden_size = 100
+output_size = 1
+model = RNNModel(vocabulary_size, hidden_size, output_size)
+
+# Loss and optimizer
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Training the model
+num_epochs = 5
+batch_size = 32
+
+# Convert to DataLoader
+train_data = TensorDataset(X_train, y_train)
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True) 
+
+# Training loop
+for epoch in range(num_epochs):
+    for inputs, labels in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}'):
+        optimizer.zero_grad()
+        outputs = model(inputs.long())
+        loss = criterion(outputs.squeeze(), labels) 
+        loss.backward()
+        optimizer.step()
+
+# Evaluation
+with torch.no_grad():
+    model.eval()
+    test_outputs = model(X_test.long())
+    predicted_labels = (test_outputs.squeeze() > 0.5).float()
+    accuracy = (predicted_labels == y_test).float().mean()
+
+print(f'Accuracy: {accuracy.item()}')
+
--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,100 @@
 # 3. Start of text and end of text token <S> <E>
 # 4. convert numbers to a number token <NUM>
 # 5. More stuff as we think of is
+
+
+import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from tqdm import tqdm #for a progress bar... removing stop words take long
+from nltk.stem import WordNetLemmatizer # issues unzipping wordnet package...
+
+
+# Load the dataset
+file_path = '../cs448_final/data/data.csv'
+columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
+df = pd.read_csv(file_path, header=None, names=columns, encoding='ISO-8859-1')
+
+def load_data(file_path):
+    columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
+    df = pd.read_csv(file_path, header=None, names=columns, encoding='ISO-8859-1')
+    return df
+
+# Data preprocessing
+def clean_text(text):
+    # Convert to lowercase
+    text = text.lower()
+ 
+     # Expand contractions
+    contractions = {"can't": "cannot", "won't": "will not", "I'm": "I am", "it's": "it is", "didn't": "did not"}
+    for contraction, expansion in contractions.items():
+        text = text.replace(contraction, expansion)
+
+    # Remove special characters and symbols
+    text = re.sub(r'[^A-Za-z0-9\s]', '', text) 
+    
+    # Remove punctuation (excluding periods)
+    text = re.sub(r'[^\w\s.]', '', text)
+    
+    # Replace periods with a token
+    text = re.sub(r'\.', ' <PERIOD> ', text)
+    
+    # Add start and end of text tokens
+    text = '<S> ' + text + ' <E>'
+    
+    # Convert numbers to a number token
+    text = re.sub(r'\b\d+\b', ' <NUM> ', text)
+    
+    return text
+
+def additional_cleaning(text):
+    
+    # Tokenize the text
+    tokens = word_tokenize(text)
+    
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.lower() not in stop_words]
+    
+    # Join the tokens back into a sentence
+    cleaned_text = ' '.join(tokens)
+    
+    return cleaned_text
+
+def removed_url(text):
+    # Remove URLs
+    text = re.sub(r'http\S+', '', text)
+    
+    # Remove user handles
+    text = re.sub(r'@[\w_]+', '', text)
+    
+    return text
+
+def lemmatize_text(text):
+    lemmatizer = WordNetLemmatizer()
+    tokens = word_tokenize(text)
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+# Vectorized cleaning
+tqdm.pandas()
+df['text'] = df['text'].apply(clean_text)
+df['text'] = df['text'].apply(additional_cleaning)
+df['text'] = df['text'].apply(removed_url)
+df['text'] = df['text'].apply(lemmatize_text) 
+
+# Display the preprocessed data
+print(df.head())
+
+
+"""This code cleans the dataset by:
+Removes stopwords
+Expanding contractions.
+Removing special characters and symbols.
+Removing URLs and user handles.
+Lemmatization using the WordNetLemmatizer from NLTK
+In additon to what Noah mentioned above.""" 
+
+
+
