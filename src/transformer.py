5cb638f23f0652bcb701a1a9415460c697b3a7b9
NoahSchiro <noahschiro@rocketmail.com>
2023-11-15 14:04:58 -0500

    basic transformer architecture set up

--- src/transformer.py
+++ src/transformer.py
@@ -1,8 +1,49 @@
+import torch
 from torch import nn
 
 class TransformerModel(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            vocab_size,  # Count of unique tokens (for embedding layer)
+            embed_size,  # What dimensionality do we want the embedding to have?
+            num_heads,   # Number of attention heads
+            num_layers,  # Number of encoding / decoding layers
+            context_size,# Size of input (230 because our max tweet size is 229)
+            num_classes
+        ):
+        super(TransformerModel, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+
+        self.transformer = nn.Transformer(
+            d_model=embed_size,
+            nhead=num_heads,
+            num_encoder_layers=num_layers,
+            num_decoder_layers=num_layers,
+            dim_feedforward=512,
+            dropout=0.1
+        )
+
+        # Linear layer for classification
+        self.fc = nn.Linear(embed_size, num_classes)
+
+        self.context_size = context_size
 
     def forward(self, x):
-        pass
+        # Assuming x is the input tensor of shape (sequence_length, batch_size)
+        # AND x must be 230 tokens long
+
+
+        # Embedding layer
+        x = self.embedding(x)
+
+        # Transformer layer
+        x = self.transformer(x, x)
+
+        # Global average pooling
+        x = x.mean(dim=0)
+
+        # Classification layer
+        x = self.fc(x)
+
+        return x
--- train_transformer.py
+++ train_transformer.py
@@ -1,4 +1,6 @@
 from src.utils import get_data_torchtext
+from src.transformer import TransformerModel
+import torch
 
 if __name__=="__main__":
 
@@ -8,11 +10,27 @@ if __name__=="__main__":
     # "It's going to be a great example" turns into:
     # ['it', "'", 's', 'going', 'to', 'be', 'a', 'great', 'example']
     # Data is the actual data of form [(target, text), ...]
-    vocab, tokenizer, data = get_data_torchtext()
+    #vocab, tokenizer, data = get_data_torchtext()
 
     # Create a function that processes text
-    text_fn  = lambda x: vocab(tokenizer(x))
+    #text_fn  = lambda x: vocab(tokenizer(x))
 
-    print(text_fn("Here is an an example string"))
+    # Maximum token length is 229
+    # Average token length is 16.56
 
-    
+    model = TransformerModel(
+        vocab_size=70000,#len(vocab),
+        embed_size=256,   # We have a decent sized vocab so I am selecting a fairly high dim
+        num_heads=8,      # Dunno, common practice
+        num_layers=8,     # Dunno, common practice
+        context_size=230, # Determined by max tweet size
+        num_classes=3     # Determined by dataset
+    )
+
+    batch = 1
+    rand = torch.randint(0, 70000, (230, batch))
+    print(rand)
+
+    out = model(rand)
+
+    print(out)
